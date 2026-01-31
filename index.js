256291d1cb4506882a893a3a5c62f49131748e05
Ariful Islam <arifulq234@gmail.com>
2026-01-31 16:22:15 +0600

    Optimized code by deleteing unneed things

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.URI;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
